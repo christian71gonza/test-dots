f4ca7fee528f9224a57a013ee3d2e6434a378156
christian71gonza <christian71gonza.les.4.878@gmail.com>
2024-12-02 11:14:46 -0300

    First commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Your new nix config";
+
+  inputs = {
+    # Nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... } @ inputs: let
+    inherit (self) outputs;
+  in {
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      # FIXME replace with your hostname
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs outputs; };
+
+        # > Our main NixOS configuration file <
+        modules = [ ./nixos/configuration.nix ];
+      };
+    };
+  };
+}
---
+++ nixos/configuration.nix
@@ -0,0 +1,183 @@
+# This is your system's configuration file.
+# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
+
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  # You can import other NixOS modules here
+  imports = [
+    # If you want to use modules from other flakes (such as nixos-hardware):
+    # inputs.hardware.nixosModules.common-cpu-amd
+    # inputs.hardware.nixosModules.common-ssd
+
+    # You can also split up your configuration and import pieces of it here:
+    # ./users.nix
+
+    # Import your generated (nixos-generate-config) hardware configuration
+    ./hardware-configuration.nix
+  ];
+
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+    };
+  };
+
+  nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+
+      # Opinionated: disable global registry
+      flake-registry = "";
+
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: { inherit flake; }) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
+
+  # FIXME: Add the rest of your current configuration
+
+  programs.firefox.enable = true;
+  programs.waybar.enable = true;
+  programs.hyprland.enable = true;
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
+
+    grub = {
+      devices = [ "nodev" ];
+      efiSupport = true;
+      enable = true;
+
+      extraEntries = ''
+        menuentry "Win 11" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          insmod chain
+          search --fs-uuid --set=root F2FA-6537
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+      '';
+    };
+  };
+
+  networking.networkmanager.enable = true;
+
+  time = {
+    timeZone = "America/Montevideo";
+    hardwareClockInLocalTime = true;
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh = {
+    enable = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+    enableCompletion = true;
+  };
+
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      safe.directory = "/home/user/.nix-dots";
+      user = {
+        email = "christian71gonza.les.4.878@gmail.com";
+        name = "christian71gonza";
+      };
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      scan_timeout = 5;
+      character = {
+        error_symbol = "[ ](bold red)";
+        success_symbol = "[ ](bold green)";
+        vicmd_symbol = "[ ](bold yellow)";
+        format = "$symbol [~](bold blue) [❯](bold red)[❯](bold yellow)[❯](bold green) ";
+      };
+      git_commit = {commit_hash_length = 4;};
+      directory = {
+      format = "";
+      };
+      line_break.disabled = false;
+      lua.symbol = "[](blue) ";
+      hostname = {
+        ssh_only = true;
+        format = "[$hostname](bold blue) ";
+        disabled = false;
+      };
+    };
+  };
+
+  # TODO: Set your hostname
+  networking.hostName = "nixos";
+
+  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
+  users.users = {
+    # FIXME: Replace with your username
+    user = {
+      isNormalUser = true;
+
+      # openssh.authorizedKeys.keys = [
+      #   # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
+      # ];
+
+      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      extraGroups = [ "wheel" "networkmanager" ];
+
+      packages = with pkgs; [
+        kitty
+        hyprpaper
+        vim
+        alacritty
+	pfetch
+        htop
+      ];
+    };
+  };
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  system.stateVersion = "23.05";
+}
