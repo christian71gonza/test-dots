a101b56f4a4d5caa0e5345898a7d39e6c2a1b63c
christian71gonza <christian71gonza.les.4.878@gmail.com>
2024-12-10 21:45:09 -0300

    Update packages.nix

--- user/packages.nix
+++ user/packages.nix
@@ -4,13 +4,6 @@
   inputs,
   ...
 }: {
-  programs = {
-    firefox.enable = true;
-    hyprland.enable = true;
-    waybar.enable = true;
-    tmux.enable = true;
-  };
-
   environment.systemPackages = with pkgs; [
     (import ./alacritty.nix {inherit pkgs;})
     kitty
